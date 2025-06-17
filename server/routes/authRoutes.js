7c9ded99120d4e37641ce1253f93f4231cf2c466
avisha rodrigoo <avizkarodrigo@gmail.com>
2025-06-17 21:54:12 +0530

    server side add
    add

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,17 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController');
-const { protect } = require('../middlewares/authMiddleware');
+const { signup, login } = require("../controllers/authControllers");
 
-router.post('/register', authController.register);
-router.post('/verify-otp', authController.verifyOtp);
-router.post('/login', authController.login);
-router.post('/google-login', authController.googleLogin);
-router.post('/forgot-password', authController.forgotPassword);
-router.post('/reset-password', authController.resetPassword);
-
-router.get('/protected', protect, (req, res) => {
-  res.json({ message: "Access granted", user: req.user });
-});
+// Routes beginning with /api/auth
+router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
